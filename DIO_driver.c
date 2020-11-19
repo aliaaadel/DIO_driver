ee24b2957f83a9cc96298b0d064d07bf5a6244b1
aliaa <aliaaadel245@gmail.com>
2020-11-19 22:53:35 +0200

    added function output , input , outputhigh and outputlow

--- DIO_driver.c
+++ DIO_driver.c
@@ -4,5 +4,19 @@
  *  Created on: Nov 19, 2020
  *      Author: aliaa
  */
+#include<DIO_driver.h>
+void makeOutput(port,pin){
+	setBit(port,pin);
+}
+
+void makeInput(port,pin){
+	clearBit(port,pin);
+}
+void outputHigh(port,pin){
+	setBit(port,pin);
+}
+void outputLow(port,pin){
+	clearBit(port,pin);
+}
 
 
