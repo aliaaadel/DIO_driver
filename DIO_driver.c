2b1e288ab9ec21db43c2680099b3b18509a8f80e
aliaa <aliaaadel245@gmail.com>
2020-11-20 15:48:54 +0200

    modified functions

--- DIO_driver.c
+++ DIO_driver.c
@@ -4,22 +4,25 @@
  *  Created on: Nov 19, 2020
  *      Author: aliaa
  */
-#include<DIO_driver.h>
-void makeOutput(reg,pin){
+#include "DIO_driver.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void makeOutput(unsigned char reg,char pin){
 	setBit(reg,pin);
 }
 
-void makeInput(reg,pin){
+void makeInput(unsigned char reg,char pin){
 	clearBit(reg,pin);
 }
-void outputHigh(reg,pin){
+void outputHigh(unsigned char reg,char pin){
 	setBit(reg,pin);
 }
-void outputLow(reg,pin){
+void outputLow( unsigned char reg,char pin){
 	clearBit(reg,pin);
 }
 
-char readInput(reg,pin){
+char readInput(unsigned char reg,char pin){
 	if (readBit(reg,pin)>1){
 		return 1;
 	}
@@ -27,9 +30,9 @@ char readInput(reg,pin){
 	return 0 ;
 	}
 }
-void turnOnPullUP(reg,pin){
+void turnOnPullUP( unsigned char reg,char pin){
 	setBit(reg,pin);
 }
-void turnOffPullUP(reg,pin){
+void turnOffPullUP( unsigned char reg,char pin){
 	clearBit(reg,pin);
 }
---
+++ Debug/DIO_driver.d
@@ -0,0 +1,3 @@
+DIO_driver.o DIO_driver.o: ../DIO_driver.c ../DIO_driver.h
+
+../DIO_driver.h:
